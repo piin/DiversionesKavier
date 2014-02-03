b1391f23a095691c1f023c099206632fa798ea8a
piin <jair.pin91@gmail.com>
2014-02-03 03:46:08 -0600

    arreglo bug de cache

--- src/com/subirtupagina/diversioneskavier/Secciones.java
+++ src/com/subirtupagina/diversioneskavier/Secciones.java
@@ -9,11 +9,12 @@ import android.app.Activity;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Window;
 import android.widget.ImageView;
 
 public class Secciones extends Activity {
-
+	private ImageLoader imageLoader;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		//setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
@@ -30,7 +31,7 @@ public class Secciones extends Activity {
 		ImageView image = (ImageView) findViewById(R.id.imgSecciones);
 		
 		// Get singletone instance of ImageLoader
-		ImageLoader imageLoader = ImageLoader.getInstance();
+		imageLoader = ImageLoader.getInstance();
 		// Initialize ImageLoader with configuration. Do it once.
 		imageLoader.init(ImageLoaderConfiguration.createDefault(this));
 		// Load and display image asynchronously
@@ -43,13 +44,20 @@ public class Secciones extends Activity {
 		 
 		 //Carga imagen de url a ImageView
 		imageLoader.displayImage(urlImagen, image, options);
-		//Limpia memoria cache del disco
-		imageLoader.clearDiscCache();
+
 	}
 	  @Override 
 	  public void onConfigurationChanged(Configuration newConfig) { 
 	      super.onConfigurationChanged(newConfig); 
 	  } 
-	
+	  
+	  public boolean onKeyDown(int keyCode, KeyEvent event) {
+	   	  
+	    if (keyCode == KeyEvent.KEYCODE_BACK) {
+			//Limpia memoria cache del disco			
+			imageLoader.clearDiscCache();
+	    }
+	    return super.onKeyDown(keyCode, event);
+	  }
 
 }
