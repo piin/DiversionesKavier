d2c8f4458ce2688db54370ff441d4a700052ee28
lexds1591 <lexds_1591@hotmail.com>
2014-02-08 00:12:37 -0600

    actualizando classes

--- gen/com/subirtupagina/diversioneskavier/R.java
+++ gen/com/subirtupagina/diversioneskavier/R.java
@@ -72,13 +72,18 @@ public final class R {
         public static final int torso=0x7f020023;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f0a0000;
-        public static final int action_settings=0x7f0a0006;
-        public static final int btnDiviertete=0x7f0a0002;
-        public static final int imgSecciones=0x7f0a0005;
-        public static final int imgTitulo=0x7f0a0003;
-        public static final int lvPublicaciones=0x7f0a0001;
-        public static final int tvTitulo=0x7f0a0004;
+        public static final int RelativeLayout1=0x7f0a0005;
+        public static final int action_settings=0x7f0a000b;
+        public static final int btnDiviertete=0x7f0a0007;
+        public static final int button1=0x7f0a0000;
+        public static final int button2=0x7f0a0001;
+        public static final int button3=0x7f0a0002;
+        public static final int button4=0x7f0a0003;
+        public static final int button5=0x7f0a0004;
+        public static final int imgSecciones=0x7f0a000a;
+        public static final int imgTitulo=0x7f0a0008;
+        public static final int lvPublicaciones=0x7f0a0006;
+        public static final int tvTitulo=0x7f0a0009;
     }
     public static final class layout {
         public static final int activity_hangman_menu=0x7f030000;
--- src/com/subirtupagina/diversioneskavier/HangmanMenu.java
+++ src/com/subirtupagina/diversioneskavier/HangmanMenu.java
@@ -1,22 +1,135 @@
 package com.subirtupagina.diversioneskavier;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class HangmanMenu extends Activity {
+	MediaPlayer bg,bt,ex,wr,bg2;
+	MediaPlayer bgStart,win,correct,error,gameover;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	//Log.i("HEY", selected);
+    	/*** Sound  ***/ 
+    	bg = MediaPlayer.create(HangmanMenu.this, R.raw.background);
+    	bt = MediaPlayer.create(HangmanMenu.this, R.raw.button);
+    	wr = MediaPlayer.create(HangmanMenu.this, R.raw.warning);
+    	ex = MediaPlayer.create(HangmanMenu.this, R.raw.exit);
+    	/**************/ 
+    	 setRequestedOrientation(ActivityInfo. SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+    	
+    	// delete bar title
+ 		requestWindowFeature(Window.FEATURE_NO_TITLE);
+          
+         //delete bar task
+         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hangman_menu);
+    }
+    @Override
+    protected void onResume(){
+    	super.onResume();
+    	bg.seekTo(0);
+    	bg.start();
+    	bg.setLooping(true);
+    }
+	public void exitAlert(View view) {
+		bt.start();
+		wr.start();
+    	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    	builder.setMessage("Are you sure you want to quit?")
+    	        .setTitle("Alert")
+    	        .setCancelable(false)
+    	        .setIcon(R.drawable.sad)
+    	        .setNegativeButton("Return",
+    	                new DialogInterface.OnClickListener() {
+    	                    public void onClick(DialogInterface dialog, int id) {
+    	                        dialog.cancel();
+    	                    }
+    	                })
+    	        .setPositiveButton("Quit",
+    	                new DialogInterface.OnClickListener() {
+    	                    public void onClick(DialogInterface dialog, int id) {
+    	                    	exit();
+    	                    }
+    	                });
+    	AlertDialog alert = builder.create();
+    	alert.show();
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_hangman_menu);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.hangman_menu, menu);
-		return true;
-	}
+    	
+    }
+    public void exit(){
+    	ex.start();
+    	while( ex.isPlaying() == true){
+    		//do nothing
+    	}
+    	System.exit(0);
+    }
+    public void start(View view){
+    	bg.pause();
+        bt.start();
+    	Intent intent = new Intent(HangmanMenu.this, SelectLanguage.class);
+    	startActivity(intent);
+    	
+    }
+    public void score(View view){
+    	bt.start();
+    	Intent intent = new Intent(this, Score.class);
+    	startActivity(intent);
+    }
+    public void soundPressed(View view){
+    	Button boton = (Button) findViewById(R.id.button4); 
+    	Button boton2= (Button) findViewById(R.id.button5);
+    	boton.setVisibility(View.INVISIBLE);
+    	boton2.setVisibility(View.VISIBLE);
+    	bt.start();
+    	bg.pause();	
+    }
+    public void notsoundPressed(View view){
+    	Button boton = (Button) findViewById(R.id.button4); 
+    	Button boton2= (Button) findViewById(R.id.button5);
+    	boton.setVisibility(View.VISIBLE);
+    	boton2.setVisibility(View.INVISIBLE);
+    	bt.start();
+    	bg.seekTo(0);
+		bg.start();
+		bg.setLooping(true);
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+      if (keyCode == KeyEvent.KEYCODE_BACK) {
+    	  wr.start();
+        new AlertDialog.Builder(this)
+          .setIcon(R.drawable.sad)
+          .setTitle("ALERT")
+          .setMessage("Are you sure you want to quit?")
+          .setNegativeButton("Return", null)//without listener
+          .setPositiveButton("Quit", new DialogInterface.OnClickListener() {//listener which close the activity
+				            @Override
+				            public void onClick(DialogInterface dialog, int which){
+				              //exit
+				              exit();
+				            }
+				          })
+          .show();
+        //if the listener returns true, means event is processing, and nobody can't do anything
+        return true;
+      }
+      //return event its own place
+      return super.onKeyDown(keyCode, event);
+    }
+    
 
 }
